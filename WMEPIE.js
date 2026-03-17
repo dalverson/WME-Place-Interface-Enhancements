332a677b871d6e604aafe782cf6d7abb73496158
justins83 <justins83.waze@gmail.com>
2026-03-16 21:02:27 -0400

    Update WMEPIE.js

--- WMEPIE.js
+++ WMEPIE.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Place Interface Enhancements
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2026.03.15.00
+// @version      2026.03.15.01
 // @description  Enhancements to various Place interfaces
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
