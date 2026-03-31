63fed127b7088fd7fb1fa552622b6863bdaa2bba
Dave Alverson <daveacincy.waze@gmail.com>
2026-03-31 14:12:24 -0400

    make Open addr on create more reliable

--- WMEPIE.js
+++ WMEPIE.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WME Place Interface Enhancements
 // @namespace    https://greasyfork.org/users/30701-justins83-waze
-// @version      2026.03.28.00
+// @version      2026.03.31.00
 // @description  Enhancements to various Place interfaces
 // @include      https://www.waze.com/editor*
 // @include      https://www.waze.com/*/editor*
@@ -2564,12 +2564,12 @@
     }
 
     async function editRPPAddress(rppTries = 1) {
-        if ($(WME_DOM.addressEditView).length > 0) {
+        if ($(WME_DOM.addressFullAddress).length > 0) {
             $(WME_DOM.addressFullAddress).trigger('click');
             await new Promise((r) => setTimeout(r, 150));
             $('input', $(WME_DOM.houseNumber)[0].shadowRoot).focus();
         } else if (rppTries < 1000) {
-            console.log('not found');
+            console.log('WMEPIE: waiting for .full-address');
             setTimeout(async function () {
                 editRPPAddress(++rppTries);
             }, 200);
